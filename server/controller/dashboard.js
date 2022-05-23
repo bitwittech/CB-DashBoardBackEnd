7bc733d4395f7ec5a7e3e903c0f5a4a409f7e4a6
Yashwant <yashwantsahu3002@gmail.com>
2022-05-23 18:18:36 +0530

    Dashboard APIs modified

--- server/controller/dashboard.js
+++ server/controller/dashboard.js
@@ -1,35 +1,70 @@
-const db = require('../../database/dbConfig.js')
+const db = require("../../database/dbConfig.js");
 
 // ======================= Tracking APis ===============================================
 
-exports.siteReport = async (req,res) =>{
+exports.siteReport = async (req, res) => {
+  const presentDate = new Date().toLocaleDateString().split("/");
 
-    const presentDate = new Date().toLocaleDateString().split('/')
+  let todayLog = 0;
+  let monthLog = 0;
+  let yearLog = 0;
 
-    let todayLog = 0;
-    let monthLog = 0;
-    let yearLog = 0;
+  // [ '2022', '05', '19' ] time
+  // [ '23', '05', '2022' ] present
 
-    // [ '2022', '05', '19' ] time 
-    // [ '23', '05', '2022' ] present
-   
-    await db.table('user_tracking_data')
-    .then((response)=>{
+  await db
+    .table("user_tracking_data")
+    .then(async (response) => {
+      response.map((data) => {
+        let time = JSON.stringify(data.time_stamp)
+          .split("T")[0]
+          .slice(1)
+          .split("-");
 
-       response.map((data)=>{
+        if (time[2] === presentDate[0]) todayLog += 1;
+        if (time[1] === presentDate[1]) monthLog += 1;
+        if (time[0] === presentDate[2]) yearLog += 1;
+      });
 
-        let time = JSON.stringify(data.time_stamp).split('T')[0].slice(1).split('-')
-        
-        if(time[2] === presentDate[0]) todayLog+=1;
-        if(time[1] === presentDate[1]) monthLog+=1;
-        if(time[0] === presentDate[2]) yearLog+=1;
+      // count user
+      await db
+        .table("newregistration")
+        .count("_id as CNT")
+        .then(async (data) => {
+          const totalUser = data[0].CNT;
+          // count free courses
+          await db
+            .table("data")
+            .whereNull("price")
+            .count("index as CNT")
+
+            .then(async (free) => {
+              const freeCourse = free[0].CNT;
+
+              await db
+                .table("data")
+                .whereNotNull("price")
+                .count("index as CNT")
+                .then(async (paid) => {
+                  const totalCourse = parseInt(paid[0].CNT) + parseInt(freeCourse);
+
+                  const paidCourse = paid[0].CNT;
+
+                  res.send({
+                    todayLog,
+                    monthLog,
+                    yearLog,
+                    totalUser,
+                    totalCourse,
+                    freeCourse,
+                    paidCourse,
+                  });
+                });
+            });
+        });
     })
-       
-      res.send({todayLog,monthLog,yearLog})
-    })
-    .catch((err)=>{
-        console.log(err)
-        res.send("Not Done !!!")
-    })
-
-}
+    .catch((err) => {
+      console.log(err);
+      res.send("Not Done !!!");
+    });
+};

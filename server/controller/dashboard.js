97a78af8df8815a615baae132a5b1ba008a69f9b
Yashwant <yashwantsahu3002@gmail.com>
2022-08-04 14:59:39 +0530

    dashboard apis improve

--- server/controller/dashboard.js
+++ server/controller/dashboard.js
@@ -1,3 +1,4 @@
+const { parse } = require("path");
 const db = require("../../database/dbConfig.js");
 
 // ======================= Tracking APis ===============================================
@@ -8,15 +9,16 @@ exports.siteReport = async (req, res) => {
   let todayLog = 0;
   let monthLog = 0;
   let yearLog = 0;
-  let anonymous  = 0;
+  let anonymous = 0;
   let todayAnonymous = 0;
   let monthAnonymous = 0;
 
   // [ '2022', '05', '19' ] time
   // [ '23', '05', '2022' ] present
+  let arr = [0,1,2]
 
   await db
-    .table("user_tracking_data").orderBy('_id','desc')
+    .table("user_tracking_data").orderBy('_id', 'desc')
     .then(async (response) => {
       response.map((data) => {
         let time = JSON.stringify(data.time_stamp)
@@ -24,39 +26,43 @@ exports.siteReport = async (req, res) => {
           .slice(1)
           .split("-");
 
-          // time[2] = (parseInt(time[2]) + 1).toString();
-          // [ '2022', '05', '12' ] [ '12', '07', '2022' ]
-        if (data.user_email !== 'User Not Logged In')
-        {
-          console.log(time,presentDate)
+        arr.map((val,index)=>{
+          time[index] = parseInt(time[index])
+          presentDate[index] = parseInt(presentDate[index])
+
+        })
+
+        // time[2] += 1;
+        // [ '2022', '05', '12' ] [ '12', '07', '2022' ]
+        if (data.user_email !== 'User Not Logged In') {
+          // console.log(time,presentDate)
           // day
-          if (time.includes(presentDate[0]) && time.includes(presentDate[1]) && time.includes(presentDate[2]) )
-              todayLog += 1;
+          if (time[2] === presentDate[0] && time[1] === presentDate[1] && time[0] === presentDate[2])
+            todayLog += 1;
           //month 
-          if (time.includes(presentDate[1]) && time.includes(presentDate[2]) )
-              monthLog += 1;
+          if (time[1] === presentDate[1] && time[0] === presentDate[2])
+            monthLog += 1;
           // year
-            if (time.includes(presentDate[2]) )
-            yearLog += 1; 
+          if (time[0] === presentDate[2])
+            yearLog += 1;
         }
         else {
-          if (time.includes(presentDate[0]) && time.includes(presentDate[1]) && time.includes(presentDate[2])) 
-          {
+          // day
+          if (time[2] === presentDate[0] && time[1] === presentDate[1] && time[0] === presentDate[2]) {
             todayAnonymous += 1;
-            anonymous+=1
+            anonymous += 1
           }
-          if (time.includes(presentDate[1]) && time.includes(presentDate[2]) ) 
-          {
+          //month 
+          if (time[1] === presentDate[1] && time[0] === presentDate[2]) {
             monthAnonymous += 1;
-            anonymous+=1
-          } 
+            anonymous += 1
+          }
           else {
-          anonymous+=1
-        }
+            anonymous += 1
+          }
         }
       });
 
-      console.log(todayLog,todayAnonymous)
 
       // count user
       await db
@@ -91,9 +97,9 @@ exports.siteReport = async (req, res) => {
                     freeCourse,
                     paidCourse,
                     anonymous,
-                    todayTraffic : todayLog + todayAnonymous,
-                    monthTraffic : monthLog + monthAnonymous, 
-                    yearTraffic : yearLog + anonymous, 
+                    todayTraffic: todayLog + todayAnonymous,
+                    monthTraffic: monthLog + monthAnonymous,
+                    yearTraffic: yearLog + anonymous,
                   });
                 });
             });
